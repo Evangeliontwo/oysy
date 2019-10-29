db2e369b785b9507a233f53b39998683ec108414
Evangeliontwo <tonny-jane@163.com>
2019-10-29 11:26:54 +0800

    333

--- src/main/java/com/kgc/oysy/cloudtest/demo/DemoApplication.java
+++ src/main/java/com/kgc/oysy/cloudtest/demo/DemoApplication.java
@@ -7,7 +7,7 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @SpringBootApplication
 @EnableEurekaServer
 public class DemoApplication {
-
+//这是注册中心
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
     }
