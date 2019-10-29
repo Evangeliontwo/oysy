cd2093e362cce5a817f5265c2ba14a53061065ff
Evangeliontwo <57120547+Evangeliontwo@users.noreply.github.com>
2019-10-29 11:24:47 +0800

    Update DemoApplication.java

--- src/main/java/com/kgc/oysy/cloudtest/demo/DemoApplication.java
+++ src/main/java/com/kgc/oysy/cloudtest/demo/DemoApplication.java
@@ -6,7 +6,7 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
-public class DemoApplication {
+public class DemoApplication {//asdf
 
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
